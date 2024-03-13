21d0b43175b1f993e48e1f0ac48390977e0bca8a
ljzimunya <146447434+ljzimunya@users.noreply.github.com>
2024-03-13 15:39:13 +0200

    added a footer with a link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Weather Project</header>
+      <footer>
+        This project was coded by Laura Zimunya and is
+        <a href="https://github.com/ljzimunya" target="_blank" rel="noreferrer">
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
