38ed37c19f958a7660a1794faa7e46a00ad7bd7c
ljzimunya <146447434+ljzimunya@users.noreply.github.com>
2024-03-14 11:33:26 +0200

    added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Weather Project</header>
-      <footer>
-        This project was coded by Laura Zimunya and is
-        <a href="https://github.com/ljzimunya" target="_blank" rel="noreferrer">
-          open-sourced on Github
-        </a>
-      </footer>
+      <div class="container">
+        <header className="App-header">Weather Project</header>
+        <footer>
+          This project was coded by Laura Zimunya and is
+          <a
+            href="https://github.com/ljzimunya"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
