ac62774b8e55bd5f8d30c60a75a9cd115a7ced3e
ljzimunya <146447434+ljzimunya@users.noreply.github.com>
2024-03-20 12:58:23 +0200

    worked on project css

--- src/Weather.js
+++ src/Weather.js
@@ -5,19 +5,35 @@ export default function Weather() {
   return (
     <div className="Weather">
       <form>
-        <input type="Serch" placeholder="Enter a city..." className="form" />
-        <input type="submit" value="Search" className="btn btn-primary" />
+        <div className="row">
+          <div className="col-6">
+            <input
+              type="Serch"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
       </form>
       <h1>Harare</h1>
       <ul>
-        <li>Wednesday, 07:00hrs </li>
+        <li>Wednesday, 07:00</li>
         <li>Sunny</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img src="" alt="sunny" />
-          6°C
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+            alt="sunny"
+          />
+          <span className="temperature">6</span>
+          <span className="unit">°C</span>
         </div>
+
         <div className="col-6">
           <ul>
             <li>Precipitation: 15%</li>
