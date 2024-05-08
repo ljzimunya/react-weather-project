e5366a691cb6177ddfc45dd8855f20feec2f850b
ljzimunya <146447434+ljzimunya@users.noreply.github.com>
2024-05-08 15:31:02 +0200

    revised code

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -17,8 +17,9 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="clearfix">
             <div className="float-left">
-              <WeatherIcon code={props.data.icon} />
+              <WeatherIcon code={props.data.icon} size={52} />
             </div>
+
             <div className="float-left">
               <WeatherTemperature celcius={props.data.temperature} />
             </div>
