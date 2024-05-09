c997298d9c0627ad772372e0cdbc0db4934f786d
ljzimunya <146447434+ljzimunya@users.noreply.github.com>
2024-05-09 21:34:09 +0200

    corrected erros in weather info file

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -21,7 +21,7 @@ export default function WeatherInfo(props) {
             </div>
 
             <div className="float-left">
-              <WeatherTemperature celcius={props.data.temperature} />
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
         </div>
